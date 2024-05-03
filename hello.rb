662629c9f9f1f3292ad55b50c68bddf486b950a1
Diogo Iwasaki <67493856+Iwaslazkis@users.noreply.github.com>
2024-05-03 15:36:11 -0500

    First commit

---
+++ hello.rb
@@ -0,0 +1,26 @@
+puts "Hello World!"
+
+4.times { puts "Chunky Bacon!" }
+
+4.times do 
+    puts "Chunky Bacon!"
+end
+
+
+42.times {
+    puts 'Forty 2'
+}
+
+4.times do
+    puts 'Wiggle Wiggle'
+  end
+
+  42.times { |i| puts "#{i} is the meaning of life, the universe, and everything." }
+
+FOO = "hello"
+puts "I like saying: #{foo}!"
+
+example_hash = {
+    key: value,
+    "abc" => "123"
+}
